48ed89099389c9e093c7c7865499d941d2679cf8
Kenneth <kennethmendez702@gmail.com>
2025-05-27 00:02:25 -0600

    pequeño repaso

--- intro-node/01-bases.js
+++ intro-node/01-bases.js
@@ -1,4 +1,10 @@
 console.log('hola mundo');
 
 //prueba de commit a github
-//otro cambio
+ //otro cambio
+
+ console.log(this);
+ console.log(globalThis);
+
+ let nombre = 'Maria';
+ console.log(nombre);
