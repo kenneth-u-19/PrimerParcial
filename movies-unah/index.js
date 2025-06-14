67ba8cdcf12f83423aa45c3dcab91d5860c8f8b7
kenneth-u-19 <karamirezm@unah.hn>
2025-06-14 00:23:48 -0600

    avance en el repaso para examen

--- movies-unah/index.js
+++ movies-unah/index.js
@@ -2,7 +2,42 @@ import express from 'express'
 import movies from '.local_db/movies.json' with {type: 'json'}
 import {Message} from 'firebase-functions/pubsub'
 
-const app = express();
+const app = express(); //para crear la aplicacion de express
+const PORT = process.env.PORT || 3000
 
-app.use(express.json);
+app.use(express.json); //se encarga de parsear el body de las peticiones, basicamente de hacer lo que esta comentado abajo
 
+// app.use((req, res, next) => {
+
+//     let body = '';
+
+//     req.on('data', (chunk) => {
+//         body += chunk.toString(); // convierte el buffer a string
+//     });
+
+//     req.on('end', () => {
+//         req.body = JSON.parse(body); // convierte el string a JSON
+//         next()
+//     })
+
+//     console.log('Middleware ejecutado');
+// })
+
+
+//rutas
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hola, mundo!</h1>')
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})  
