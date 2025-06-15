39c890930073041997ca406861373497fb076644
kenneth-u-19 <karamirezm@unah.hn>
2025-06-15 17:33:56 -0600

    practica y tarea sin puntaje hecha

--- movies-unah/index.js
+++ movies-unah/index.js
@@ -38,10 +38,27 @@ app.get('/movies/search', (req, res) => {
 
     const {genre, year} = req.query
 
-    if(genre){
+    if(genre && year){
+        const moviesFiltered = movies.filter((movie) => {
+            //filtrar por genero y anio
+            const genreValidated = movie.genre.some((valor) => valor.toLowerCase().trim() === genre.toLowerCase().trim() )
+
+            return genreValidated && movie.year === parseInt(year)
+        })
+
+        res.json(moviesFiltered)
+
+    } else if(genre){
         //filtrar por el genero
         const moviesFiltered = movies.filter((movie) => {
             return movie.genre.some((valor) => valor.toLowerCase().trim() === genre.toLowerCase().trim())
+        })
+
+        res.json(moviesFiltered)
+    } else if(year){
+        //filtrar por anio
+        const moviesFiltered = movies.filter((movie) => {
+            return movie.year === parseInt(year)
         })
 
         res.json(moviesFiltered)
@@ -53,8 +70,46 @@ app.get('/movies/search', (req, res) => {
 })
 
 
+app.get('/movies/:id', (req, res) => {
+    const {id} = req.params
 
+    const parsedID = Number(id)
 
+    if(isNaN(parsedID)){
+        res.status(400).json({
+            message: 'El id debe ser un numero'
+        })
+    }
+
+    const movie = movies.find(({id}) => {
+        return id === parsedID
+    })
+
+    if(!movie){
+        res.status(404).json({
+            message: 'La pelicula no existe'
+        })
+    }
+
+    res.json(movie)
+})
+
+app.post('/movies', (req, res)=>{
+    const id = Date.now()//genera un id unico basado en la fecha actual
+
+    req.body.id = id
+    movies.push(req.body)
+
+    res
+    .status(201) //establece el codigo de estado http como 201 (creado)
+    .json(req.body)
+})
+
+//actualizar
+app.put('/movies/:id', (req, res)=>{})
+
+//eliminar
+app.delete('/movies/:id', (req, res)=>{})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
