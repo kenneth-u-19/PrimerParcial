48f3e43172bc37999583152ac6e1a77a83d516a2
kenneth-u-19 <karamirezm@unah.hn>
2025-06-15 11:38:51 -0600

    cambios para que funcionara como debe, eran errores de sintaxis en las importaciones del index.js

--- movies-unah/index.js
+++ movies-unah/index.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import movies from '.local_db/movies.json' with {type: 'json'}
+import movies from './local_db/movies.json' with {type: 'json'}
 import {Message} from 'firebase-functions/pubsub'
 
 const app = express(); //para crear la aplicacion de express
 const PORT = process.env.PORT || 3000
 
-app.use(express.json); //se encarga de parsear el body de las peticiones, basicamente de hacer lo que esta comentado abajo
+app.use(express.json()); //se encarga de parsear el body de las peticiones, basicamente de hacer lo que esta comentado abajo
 
 // app.use((req, res, next) => {
 
@@ -30,9 +30,27 @@ app.get('/', (req, res) => {
     res.send('<h1>Hola, mundo!</h1>')
 })
 
+app.get('/movies', (req, res) => {
+    res.json(movies)
+})
 
+app.get('/movies/search', (req, res) => {
 
+    const {genre, year} = req.query
 
+    if(genre){
+        //filtrar por el genero
+        const moviesFiltered = movies.filter((movie) => {
+            return movie.genre.some((valor) => valor.toLowerCase().trim() === genre.toLowerCase().trim())
+        })
+
+        res.json(moviesFiltered)
+    }
+
+    res.status(404).json({
+        message: 'No se ha proporcionado un genero para filtrar por las peliculas'
+    })
+})
 
 
 
